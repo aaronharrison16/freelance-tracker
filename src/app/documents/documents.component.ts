53ca3f161ef0541467e4c37e213362ef7d09ce76
Aaron Harrison <aaronharrison16@gmail.com>
2018-08-23 23:16:25 -0600

    Added mock data for docs

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
   selector: 'documents',
   templateUrl: 'documents.component.html'
 })
 export class DocumentsComponent {
-  
+  documents: Document[] = [
+    {
+      title: 'My first doc',
+      description: "asdfasdfasdf asdf",
+      file_url: 'http://google.com',
+      updated_at: '8/23/18',
+      image_url: 'http://google.com'
+    },
+    {
+      title: 'My second doc',
+      description: "asdfasdfasdf asdf",
+      file_url: 'http://google.com',
+      updated_at: '8/23/18',
+      image_url: 'http://google.com'
+    },
+    {
+      title: 'My third doc',
+      description: "asdfasdfasdf asdf",
+      file_url: 'http://google.com',
+      updated_at: '8/23/18',
+      image_url: 'http://google.com'
+    }
+  ]
 }
