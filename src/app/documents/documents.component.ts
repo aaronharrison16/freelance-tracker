98677b0eff6ecefef78da5306be45fa11a22035f
Aaron Harrison <aaronharrison16@gmail.com>
2018-08-28 22:55:52 -0600

    finalized styles for docs, as well as tied the angular app to the rails doc microservice

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,34 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Document } from './document';
-
+import { DocumentService } from './document.service';
+import { timer } from 'rxjs';
+import { timeInterval, pluck, take} from 'rxjs/operators';
 @Component({
-  selector: 'documents',
-  templateUrl: 'documents.component.html',
-  styleUrls: ['documents.component.css']
+    selector: 'documents',
+    templateUrl: 'documents.component.html',
+    styleUrls: ['documents.component.css'],
+    providers: [ DocumentService ]
 })
-export class DocumentsComponent {
-  pageTitle: string = 'Documents Dashboard'
-  documents: Document[] = [
-    {
-      title: 'My first doc',
-      description: "asdfasdfasdf asdf",
-      file_url: 'http://google.com',
-      updated_at: '8/23/18',
-      image_url: 'https://spzone-simpleprogrammer.netdna-ssl.com/wp-content/uploads/2017/07/Freelancer-What-You-Want-768x432.png'
-    },
-    {
-      title: 'My second doc',
-      description: "asdfasdfasdf asdf",
-      file_url: 'http://google.com',
-      updated_at: '8/23/18',
-      image_url: 'https://spzone-simpleprogrammer.netdna-ssl.com/wp-content/uploads/2017/07/Freelancer-What-You-Want-768x432.png'
-    },
-    {
-      title: 'My third doc',
-      description: "asdfasdfasdf asdf",
-      file_url: 'http://google.com',
-      updated_at: '8/23/18',
-      image_url: 'https://spzone-simpleprogrammer.netdna-ssl.com/wp-content/uploads/2017/07/Freelancer-What-You-Want-768x432.png'
+export class DocumentsComponent implements OnInit {
+    pageTitle: string = "Document Dashboard"
+    documents: Document[];
+    errorMessage: string;
+    mode = "Observable";
+    constructor(
+        private documentService: DocumentService,
+    ) {}
+    ngOnInit() {
+        var sourcef = timer(200, 100)
+      .pipe(
+        timeInterval(),
+        pluck('interval'),
+        take(3)
+  )
+        sourcef.subscribe(() => this.getDocuments());
     }
-  ]
+    getDocuments() {
+        this.documentService.getDocuments()
+                .subscribe(
+                    documents => this.documents = documents,
+                    error => this.errorMessage = <any>error
+                );
+    }
 }
